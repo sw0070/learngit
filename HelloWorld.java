ff60794ba91167096ba1f6647602a5cbad8f1ec1
Liusiwei <61324465+sw0070@users.noreply.github.com>
2021-05-13 12:13:12 +0800

    Create HelloWorld.java
    使用一下，创建一个文件

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+
+//creat a new file
+
+public class HelloWorld{
+  public statuc void main(String[] args){
+    System.out.println("Hello, world!");
+  }
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
